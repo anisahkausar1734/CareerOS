283754df8793e2cfbb13777e5db42c99a8a297e4
Anisah Kausar <anisahkausar1734@gmail.com>
2026-06-03 09:08:15 +0530

    Setup backend package structure

---
+++ backend/src/test/java/com/careeros/careeros_backend/CareerosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careeros.careeros_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
