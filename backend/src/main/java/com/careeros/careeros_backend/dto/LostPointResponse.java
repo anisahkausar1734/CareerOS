b5472fbc4c44dfcbec2cff6c3c01132e85fbbc09
Anisah Kausar <anisahkausar1734@gmail.com>
2026-06-07 13:55:03 +0530

    feat: add lost point engine foundation

---
+++ backend/src/main/java/com/careeros/careeros_backend/dto/LostPointResponse.java
@@ -0,0 +1,19 @@
+package com.careeros.careeros_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LostPointResponse {
+
+    private Integer totalPointsLost;
+
+    private List<LostPoint> lostPoints;
+}
---
+++ backend/src/main/java/com/careeros/careeros_backend/service/LostPointService.java
@@ -0,0 +1,10 @@
+package com.careeros.careeros_backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LostPointService {
+
+}
